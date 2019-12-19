c2008a21cea7cc9184f72180d565fb6ff233aedf
gongmeng <18588264128@163.com>
2019-12-19 02:56:44 +0000

    邮箱认证

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            //注册邮箱认证路由
+            \App\Http\Middleware\EnsureEmailIsVerified::class,
         ],
 
         'api' => [
