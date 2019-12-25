be141f88db06095439a9ba4e7d3240c37e4a5c94
gongmeng <18588264128@163.com>
2019-12-25 02:26:47 +0000

    限制头像分辨率

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -15,7 +15,7 @@
       </div>
 
       <div class="card-body">
-        <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data">
+        <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data".>
           <input type="hidden" name="_method" value="PUT">
           <input type="hidden" name="_token" value="{{ csrf_token() }}">
            @include('shared._error')
