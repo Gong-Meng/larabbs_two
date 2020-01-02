73c34680498514c4d5780c5f3ea011cc3a77459c
gongmeng <18588264128@163.com>
2020-01-02 06:46:34 +0000

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,5 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 //显示分类
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+//上传图片
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
