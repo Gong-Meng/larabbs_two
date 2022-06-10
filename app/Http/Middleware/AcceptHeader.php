8ba656af74db6498103f98a600374afc5ebb2c15
Gong Meng <18588264128@163.com>
2022-06-10 12:48:17 +0800

    [api] api基础配置

--- app/Http/Middleware/AcceptHeader.php
+++ app/Http/Middleware/AcceptHeader.php
@@ -15,6 +15,8 @@ class AcceptHeader
      */
     public function handle($request, Closure $next)
     {
+        $request->headers->set('Accept', 'application/json');
+
         return $next($request);
     }
 }
