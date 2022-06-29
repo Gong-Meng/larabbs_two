1e25e05ae0593c1136859dca1a23c819ff0a9bec
Gong Meng <18588264128@163.com>
2022-06-29 17:18:00 +0800

    用户信息

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,7 @@ class User extends Authenticatable implements  MustVerifyEmailContract, JWTSubje
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'weixin_openid', 'weixin_unionid'
     ];
 
     /**
